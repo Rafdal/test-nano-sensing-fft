7ca38ba2374e5828d22d5228f90df65b7051fbd9
Rafael Dalzotto <rafaeldalzotto.contacto@gmail.com>
2020-11-17 12:59:04 -0300

    working

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
 
 #define CHANNEL A0
-#define SAMPLING 256
+#define SAMPLING 512
 const uint16_t samples = SAMPLING;		   //This value MUST ALWAYS be a power of 2
 const double samplingFrequency = 900; //Hz, must be less than 10000 due to ADC
 
@@ -23,17 +23,21 @@ double vImag[samples];
 #include "gyro_util.h"
 #include "sd_util.h"
 
+#define MAX_VUELTAS 50
+uint8_t vueltas;
+
 void setup()
 {
 	Serial.begin(921600);
+	pinMode(13, OUTPUT);
+	digitalWrite(13, HIGH);
 	sampling_period_us = round(1000000 * (1.0 / samplingFrequency));
-	while (!Serial);
-	Serial.println("Started");
-	delay(500);
+	delay(5000);
 
-	sdSetup(4);
 	gyroSetup();
+	sdSetup(4);
 	
+	digitalWrite(13, LOW);
 }
 imu_data_t IMUdata;
 void loop()
@@ -55,6 +59,16 @@ void loop()
 	fft_data_t data = CalcFFT(vReal, vImag, CHANNEL, sampling_period_us, samples);
 	
 	sdLog(data);
-			  /* Repeat after delay */
-	// imu_data_t gyro = readGyro();
+	vueltas++;
+	// Serial.println(vueltas); 
+	if (vueltas >= MAX_VUELTAS)
+	{
+		for(;;){
+			digitalWrite(13, HIGH);
+			delay(1000);
+			digitalWrite(13, LOW);
+			delay(1000);
+			NRF_WDT->RR[0] = WDT_RR_RR_Reload; 
+		}
+	}
 }
--- src/sd_util.h
+++ src/sd_util.h
@@ -7,27 +7,64 @@ void sdSetup(uint8_t cs_pin)
     digitalWrite(cs_pin, LOW);   // Activar SD
     if (!SD.begin(cs_pin)) {
         Serial.println(F("SD card failed, or not present"));
-        while (1);
+        pinMode(LED_BUILTIN, OUTPUT);
+        for (;;){
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(100);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(100);
+            NRF_WDT->RR[0] = WDT_RR_RR_Reload;       
+        }
     }
-    Serial.println(F("SD card initialized."));
+    Serial.println(F("SD card initialized.\n"));
 
     if (SD.exists("dataset.csv"))
         SD.remove("dataset.csv");
+
+    File dataFile = SD.open("dataset.csv", FILE_WRITE);
+
+
+    if (dataFile) 
+    {
+        for (uint16_t i = 0; i < SAMPLING/2; i++)
+        {
+            dataFile.print(i);
+            Serial.print(i);
+            if (i < (SAMPLING/2)-1)
+            {
+                dataFile.print(',');
+                Serial.print(',');
+            }            
+        }
+        dataFile.println();
+        Serial.println();
+    }
+    else
+    {
+        Serial.println(F("Error abriendo el archivo"));
+    }
+    dataFile.close();
 }
 
 
 void sdLog(fft_data_t fft)
 {
-    // unsigned long us = micros();
+    unsigned long us = micros();
     File dataFile = SD.open("dataset.csv", FILE_WRITE);
     if (dataFile) 
     {
         for (uint16_t i = 0; i < fft.size; i++)
         {
             dataFile.print((int)(fft.data[i]));
-            dataFile.print(',');
+            Serial.print((int)(fft.data[i]));
+            if (i < fft.size-1)
+            {
+                dataFile.print(',');
+                Serial.print(',');
+            }            
         }
         dataFile.println();
+        Serial.println();
     }
     else
     {
